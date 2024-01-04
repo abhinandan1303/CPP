2606dfeca431f5281d33c6ea6d5489225b88cddf
abhinandan1303 <155632219+abhinandan1303@users.noreply.github.com>
2024-01-04 17:19:58 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello world\n";
+    int a , b;
+    cin >> a >> b;
+    cout << "the sun of a and b is: " << (a+b);
+
+}
